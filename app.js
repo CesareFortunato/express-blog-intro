6c9bb7c7f5cef95b4f5fc8b2d1c6645f26bf0d47
CesareFortunato <cesare.fortunato89@gmail.com>
2026-02-05 19:39:43 +0100

    fix: correct path of images

--- app.js
+++ app.js
@@ -6,42 +6,44 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
+    
+})
+
+app.get('/bacheca', (req, res) => {
     const posts = [
         {
             title: ("Ciambellone"),
             content: ("ricetta ciambellone"),
-            image: ("/public/images/ciambellone.jpeg"),
+            image: ("/images/ciambellone.jpeg"),
             tags: ["dolce"]
         },
         {
             title: ("Cracker alla barbabietola"),
             content: ("ricetta cracker alla barbabietola"),
-            image: ("/public/images/cracker_barbietola.jpeg"),
+            image: ("/images/cracker_barbietola.jpeg"),
             tags: ["pane", "verdura"]
         },
         {
             title: ("Pane fritto dolce"),
             content: ("ricetta pane fritto dolce"),
-            image: ("/public/images/pane_fritto_dolce.jpeg"),
+            image: ("/images/pane_fritto_dolce.jpeg"),
             tags: ["pane", "dolce"]
         },
         {
             title: ("Pasta con barbabietole"),
             content: ("ricetta pasta con barbabietole"),
-            image: ("/public/images/pasta_barbabietola.jpeg"),
+            image: ("/images/pasta_barbabietola.jpeg"),
             tags: ["pasta", "verdura"]
         },
         {
             title: ("Torta paesana"),
             content: ("ricetta torta paesana"),
-            image: ("/public/images/torta_paesana.jpeg"),
+            image: ("/images/torta_paesana.jpeg"),
             tags: ["dolce"]
         }
     ]
-})
 
-app.get('/bacheca', (req, res) => {
-    res.send('bacheca del mio blog')
+    res.send(posts)
 })
 
 app.listen(port, () => {
